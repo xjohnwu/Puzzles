75c83289d8802c7bb297b5bdc4563838d1dee195
xjohnwu <xjohnwu@gmail.com>
2014-01-16 23:17:05 +0000

    Postal service puzzle solution

---
+++ PostalServicePuzzle/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostalServicePuzzle
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Extensions.test();
+			Console.ReadLine();
+			
+			Stamps stamps = new Stamps(new[] {
+				0.01, 0.02, 0.03, 0.04, 0.05, 0.10, 
+				0.24, 0.37, 0.39, 0.41, 0.48, 0.60, 
+				0.63, 0.70, 0.75, 0.80, 0.83, 0.84,
+				0.87, 1.00, 3.85, 4.05, 4.60, 5.00, 14.4 });
+
+			double i = 0.06;
+			int numOfStamps;
+			do
+			{
+				numOfStamps = stamps.CalculateMinNumOfStampsNeeded(i);
+				Console.WriteLine("{0:0.00} needs {1} stamps", i, numOfStamps);
+				i += 0.01;
+			} while (numOfStamps < 11);
+
+			Console.Write("Press Enter to exit...");
+			Console.ReadLine();
+		}
+	}
+
+	public static class Extensions
+	{
+		public static string ToUpper(this string s)
+		{
+			return s;
+		}
+
+		public static void test()
+		{
+			string s = "Test";
+			Console.WriteLine(s.ToUpper());
+		}
+	}
+}
---
+++ PostalServicePuzzle/Stamps.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostalServicePuzzle
+{
+	class Stamps
+	{
+		private int[] _numOfStamps = new int[10000];
+
+
+		public Stamps(IEnumerable<double> stamps)
+		{
+			InitialiseAvailableStamps(stamps);
+		}
+
+		private void InitialiseAvailableStamps(IEnumerable<double> stamps)
+		{
+			foreach (var stamp in stamps)
+			{
+				_numOfStamps[GetIndex(stamp)] = 1;
+			}
+		}
+
+		private static int GetIndex(double postage)
+		{
+			int index = -1;
+			if (postage >= 0.01D)
+			{
+				index = Convert.ToInt32(postage * 100) - 1;
+			}
+			return index;
+		}
+
+		/// <summary>
+		/// Assuming that all the index below that postage is already calculated
+		/// </summary>
+		/// <param name="postage"></param>
+		public int CalculateMinNumOfStampsNeeded(double postage)
+		{
+			int index = GetIndex(postage);
+			int num = 0;
+			if (index >= 0)
+			{
+				for (int i = index / 2; i >= 0; i--)
+				{
+					int subStepA = _numOfStamps[i];
+					int subStepB = _numOfStamps[index - i - 1];
+
+					int totalStep = subStepA + subStepB;
+
+					if (_numOfStamps[index] == 0)
+					{
+						_numOfStamps[index] = totalStep;
+					}
+					else
+					{
+						_numOfStamps[index] = Math.Min(_numOfStamps[index], totalStep);
+					}
+				}
+
+				num = _numOfStamps[index];
+			}
+			
+			return num;
+		}
+	}
+}
